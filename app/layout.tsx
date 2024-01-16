323a9b59f579b19a185c44851edb1e0b6c864e9f
Daniel Salazar <jdps106@hotmail.com>
2024-01-15 22:26:03 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './ui/global.css'
+import { roboto } from './ui/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Roboto, Lusitana, Montserrat} from "next/font/google";
+
+export const roboto = Roboto({subsets:['latin'],weight:'100'})
+export const lusitana = Lusitana({subsets:['latin'], weight:"400"})
