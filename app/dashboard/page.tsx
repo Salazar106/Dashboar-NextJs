f633b17eb3d16c5abf81d1d03d552f2d9b1dc33c
Daniel Salazar <jdps106@hotmail.com>
2024-01-23 16:48:30 -0500

    se agrega auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,69 @@
+'use client'
 
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+  UserIcon,
+  ChartBarIcon,
+  PlusIcon
+} from '@heroicons/react/24/outline';
+import { usePathname } from "next/navigation"
+import Link from 'next/link';
 
+const links = [
+  { name: 'Valances', href: '/dashboard/valances',  icon: ChartBarIcon },
+  { name: 'Ventas',   href: '/dashboard/invoices',  icon: DocumentDuplicateIcon },
+  { name: 'Usuarios', href: '/dashboard/customers', icon: UserGroupIcon },
+];
 export default async function dashboard() {
+  const pathname = usePathname()
+
   return (
     <main className='w-full h-full'>
-      <div className="w-full p-2 bg-gray-400 text-center">
+      <div className="w-full p-2 text-center flex gap-4 justify-center flex-wrap">
+
+
+        {links.map((link) => {
+          const LinkIcon = link.icon;
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="w-32 h-32 border-2 rounded-lg flex justify-center flex-wrap border-cyan-500/50 shadow-lg shadow-gray-400"
+            >
+              <LinkIcon className="w-20 text-cyan-900" />
+              <p className="hidden  md:block">{link.name}</p>
+            </Link>
+          )
+        })}
+
+
       </div>
-        
+
     </main>
   )
 }
+2
+// export default function NavLinks() {
+//   const pathname = usePathname()
+//   return (
+//     <>
+//       {links.map((link) => {
+//         const LinkIcon = link.icon;
+//         return (
+//           <Link
+//             key={link.name}
+//             href={link.href}
+//             className={`flex h-[48px] grow items-center  justify-center gap-2 rounded-md bg-gray-100 p-3 text-sm text-black font-large hover:bg-sky-600 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3
+//             ${pathname === link.href ? 'bg-sky-600 text-white':''}
+//             `}
+//           >
+//             <LinkIcon className="w-6" />
+//             <p className="hidden  md:block">{link.name}</p>
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,11 +3,13 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
+import { signOut } from '@/auth';
+
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col  ">
-      <Link className="flex h-20 items-center justify-center  bg-sky-900 p-4 md:h-40" href="/">
+      <Link className="flex h-20 items-center justify-center bg-gradient-to-t from-cyan-50 via-cyan-500 to-blue-900   p-4 md:h-40" href="/">
         <div className="w-25 text-white items-center md:w-40">
           <Image className='justify-center' width={120} height={120} src={'/favicon.ico'} alt={'hola'} />
           {/* <AcmeLogo /> */}
@@ -16,7 +18,10 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 bg-gray-100 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form action={async () => {
+            'use server';
+            await signOut();
+          }}>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-rose-600 hover:text-blue-50 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Cerrar Sesión</div>
